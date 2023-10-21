d38ee2d8493c29cf088ec153696ba600dbe6bf3a
YargKlnc <yarg79@hotmail.com>
2023-10-21 17:39:18 -0400

    commit 3rd

--- Employee_Schema.sql
+++ Employee_Schema.sql
@@ -1,43 +1,55 @@
 --- creating tables for all csv files
 
-CREATE TABLE departments (
-	dept_no VARCHAR PRIMARY KEY,
-	dept_name VARCHAR
+CREATE TABLE titles (
+	title_id VARCHAR(50) PRIMARY KEY,
+	title VARCHAR(50)
 );
 
-CREATE TABLE dept_emt (
-	emp_no VARCHAR PRIMARY KEY,
-	dept_no VARCHAR
-);
-
-CREATE TABLE dept_manager (
-	dept_no VARCHAR,
-	emp_no VARCHAR
-);
+SELECT * FROM titles;
+DROP TABLE titles;
 
 CREATE TABLE employees (
-	emp_no VARCHAR PRIMARY KEY,
-	emp_title_id VARCHAR,
-	birth_date DATE,
-	first_name VARCHAR,
-	last_name VARCHAR,
-	sex VARCHAR,
-	hire_date DATE
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR(10) REFERENCES titles(title_id),
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	sex VARCHAR(5) NOT NULL,
+	hire_date DATE NOT NULL
 );
 
-CREATE TABLE salaries (
-	emp_no VARCHAR PRIMARY KEY,
-	salary INT
-);
+SELECT * FROM employees;
+DROP TABLE employees;
 
-CREATE TABLE titles (
-	title_id VARCHAR PRIMARY KEY,
-	title VARCHAR
+CREATE TABLE departments (
+	dept_no VARCHAR(50) PRIMARY KEY,
+	dept_name VARCHAR(50)
 );
 
 SELECT * FROM departments;
-SELECT * FROM dept_emt;
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(10) REFERENCES departments(dept_no),
+	emp_no INT
+);
+
 SELECT * FROM dept_manager;
-SELECT * FROM employees;
+
+CREATE TABLE dept_emp (
+	emp_no INT REFERENCES employees(emp_no),
+	dept_no VARCHAR(10) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE salaries (
+	emp_no INT REFERENCES employees(emp_no),
+	salary INT
+);
+
 SELECT * FROM salaries;
-SELECT * FROM titles;
+DROP TABLE salaries;
+
+
+
+
+
+
