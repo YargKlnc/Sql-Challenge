3404a8bfbdd6d7fb8c31f653020e23438c418293
YargKlnc <yarg79@hotmail.com>
2023-10-22 16:11:04 -0400

    another commit code and employeescsv

--- Employee_Schema.sql
+++ Employee_Schema.sql
@@ -1,5 +1,6 @@
 --- creating tables for all csv files
 
+-- TABLE DONE
 CREATE TABLE titles (
 	title_id VARCHAR(50) PRIMARY KEY,
 	title VARCHAR(50)
@@ -9,13 +10,15 @@ SELECT * FROM titles;
 DROP TABLE titles;
 
 CREATE TABLE employees (
-	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR(10) REFERENCES titles(title_id),
-	birth_date DATE NOT NULL,
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
-	sex VARCHAR(5) NOT NULL,
-	hire_date DATE NOT NULL
+    emp_no INT NOT NULL,
+    emp_title_id VARCHAR NOT NULL,
+	birth_date date,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex VARCHAR NOT NULL,
+    hire_date date,
+    FOREIGN KEY (emp_title_id) REFERENCES titles (title_id),
+    PRIMARY KEY (emp_no)
 );
 
 SELECT * FROM employees;
@@ -28,6 +31,7 @@ CREATE TABLE departments (
 
 SELECT * FROM departments;
 
+
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(10) REFERENCES departments(dept_no),
 	emp_no INT
@@ -35,10 +39,12 @@ CREATE TABLE dept_manager (
 
 SELECT * FROM dept_manager;
 
+
 CREATE TABLE dept_emp (
 	emp_no INT REFERENCES employees(emp_no),
 	dept_no VARCHAR(10) REFERENCES departments(dept_no)
 );
+SELECT * FROM dept_emp;
 
 CREATE TABLE salaries (
 	emp_no INT REFERENCES employees(emp_no),
